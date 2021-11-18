ebe32b446dcb1bb2098379937f84e45dc0f2c529
Brun096 <Brunoh96@outlook.it>
2021-11-18 09:43:16 +0100

    esercizio media

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -5,7 +5,7 @@ public class CibiPreferiti {
 	public static void main(String[]args) {
 		
 
-		String[] cibi= {"Lasagna", "Salmone","Mela","Tiramiù","Hamburger","Risotto allo Zafferano","Filetto di Tonno","Fegato alla veneziana"};
+		String[] cibi= {"Lasagna", "Salmone","Mela","Tiramiù","Hamburger","Risotto allo Zafferano","Filetto di Tonno"};
 		int cibiElementi=cibi.length;
 		
 		//int ciboMezzaClass=cibiElementi/2;
@@ -13,8 +13,14 @@ public class CibiPreferiti {
 		System.out.println("Il mio cibo preferito è: "+cibi[0]);
 		System.out.println("Il cibo che non mi piace è: "+cibi[cibiElementi - 1]);
 		
-		int ciboMed=cibiElementi/2;
-		System.out.println("il cibo in mezzo agli elementi è: "+cibi[ciboMed]);
+		int ciboMed;
 		
-	}
+		if(cibiElementi % 2 == 0) {
+			
+			ciboMed=cibiElementi/2;
+		}else {
+			ciboMed=(cibiElementi+1)/2;
+		}
+		System.out.println("il cibo in mezzo agli elementi è: "+cibi[ciboMed - 1]);
+		}
 }
